90e34e6bb65d31937ceefbae866be7be80b04a67
saiteja.palegar@gmail.com <saiteja.palegar@gmail.com>
2024-03-15 16:00:26 +0530

    added view questions in ADmin panel

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -10,6 +10,10 @@ const AdminPage = () => {
                 <button type="button" class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
                     <Link to={`/create`}>Create User</Link></button>
             </center>
+            <center>
+                <button type="button" class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                    <Link to={`/question_1`}>Question 1</Link></button>
+            </center>
         </div>
     );
 };
